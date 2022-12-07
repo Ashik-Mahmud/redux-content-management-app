9d8d171c7fca292e39d2884fdb2faaf5ce53e919
ashikmahmud <ashikmamud187@gmail.com>
2022-12-08 01:11:38 +0600

    added lttle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes/mainRoute";
+import Header from "./shared/Header";
 
 function App() {
   return (
-    <>
+    <main className="font-poppins">
+      <Header />
       <RouterProvider router={router} />
-    </>
+    </main>
   );
 }
 
