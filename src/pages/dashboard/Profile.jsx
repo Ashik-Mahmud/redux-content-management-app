aa1384523e61fe7b83b49b8cefb188506fbc0c65
ashikmahmud <ashikmamud187@gmail.com>
2022-12-09 22:27:01 +0600

    write little in profile section

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div className="p-10">
+        <h2 className="text-2xl my-2">Profile</h2>
+
+        <div className="flex flex-col gap-2">profile not found</div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
