d49568bce2c1467cb14ceb05e6721479ab218250
ashikmahmud <ashikmamud187@gmail.com>
2022-12-11 02:53:07 +0600

    create options to add tags and delte as well

--- src/pages/dashboard/AddBlog.jsx
+++ src/pages/dashboard/AddBlog.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { BiX } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addBlogSuccess } from "../../redux/ActionCreator/BlogCreator";
@@ -7,6 +8,7 @@ import { addBlogSuccess } from "../../redux/ActionCreator/BlogCreator";
 const AddBlog = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [tags, setTags] = useState([]);
   const {
     handleSubmit,
     register,
@@ -21,6 +23,7 @@ const AddBlog = () => {
       _id: blogs?.blogs?.length + 1,
       date: new Date(),
       ...data,
+      tags,
     };
 
     dispatch(addBlogSuccess(sendingData));
@@ -36,7 +39,9 @@ const AddBlog = () => {
           onSubmit={handleBlogSave}
         >
           <div className="form-group flex items-stretch w-full gap-2 flex-col">
-            <label htmlFor="title">Author</label>
+            <label htmlFor="title">
+              Author <span className="text-red-500">*</span>
+            </label>
             <input
               type="text"
               className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-blue-500"
@@ -51,7 +56,9 @@ const AddBlog = () => {
             )}
           </div>
           <div className="form-group flex items-stretch w-full gap-2 flex-col">
-            <label htmlFor="title">Title</label>
+            <label htmlFor="title">
+              Title <span className="text-red-500">*</span>
+            </label>
             <input
               type="text"
               className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-blue-500"
@@ -64,7 +71,9 @@ const AddBlog = () => {
             )}
           </div>
           <div className="form-group flex items-stretch w-full gap-2 flex-col">
-            <label htmlFor="Category">Category</label>
+            <label htmlFor="Category">
+              Category <span className="text-red-500">*</span>
+            </label>
             <select
               className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-blue-500 cursor-pointer"
               id="Category"
@@ -82,7 +91,9 @@ const AddBlog = () => {
             )}
           </div>
           <div className="form-group flex flex-col items-start w-full gap-2">
-            <label htmlFor="content">Content</label>
+            <label htmlFor="content">
+              Content <span className="text-red-500">*</span>
+            </label>
             <textarea
               className="form-control w-full p-5 border border-gray-300 rounded-md focus:outline-blue-500"
               id="content"
@@ -96,7 +107,9 @@ const AddBlog = () => {
             )}
           </div>
           <div className="form-group flex flex-col gap-2 w-full">
-            <label htmlFor="image">Image</label>
+            <label htmlFor="image">
+              Image <span className="text-red-500">*</span>
+            </label>
             <input
               type="url"
               className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-blue-500"
@@ -108,6 +121,54 @@ const AddBlog = () => {
               <span className="text-red-500 my-1">Image field is required</span>
             )}
           </div>
+
+          <div className="form-group flex flex-col gap-2 w-full">
+            <label htmlFor="tags">
+              Tags
+              <small>
+                (write single tag and press the <b>shift/Escape/Tab</b> to add)
+              </small>
+            </label>
+            {tags?.length > 0 && (
+              <div className="flex flex-wrap gap-2 ">
+                {tags?.map((tag, index) => (
+                  <div
+                    className="bg-gray-200  rounded-md text-sm flex items-stretch gap-2 overflow-hidden"
+                    key={index}
+                  >
+                    <span className="p-2">{tag}</span>
+                    <span
+                      onClick={() => {
+                        const newTags = tags.filter((t) => t !== tag);
+                        setTags(newTags);
+                      }}
+                      className="bg-gray-300 p-1 grid place-items-center cursor-pointer px-2"
+                    >
+                      <BiX />
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
+            <input
+              type="text"
+              className="form-control w-full p-3 border border-gray-300 rounded-md focus:outline-blue-500"
+              id="tags"
+              onKeyDown={(e) => {
+                if (
+                  e.key === "Shift" ||
+                  e.key === "Tab" ||
+                  e.key === "Escape"
+                ) {
+                  if (tags?.includes(e.target.value)) return;
+                  setTags([...tags, e.target.value]);
+                  e.target.value = "";
+                }
+              }}
+              placeholder="Enter Tags"
+            />
+          </div>
+
           <button
             type="submit"
             className="btn btn-primary px-5 py-3 bg-blue-500 text-white rounded-sm"
